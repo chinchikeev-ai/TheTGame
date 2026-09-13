0a34f7ce1b1e18f9df2d79e2f72551a051d81d67
chinchikeev-ai <chinchikeev@gmail.com>
2026-09-13 18:49:16 +0800

    refactor: remove shield visuals from gameplay factory

--- Assets/Game/Heroes/Hector/HectorShieldWallFactory.cs
+++ Assets/Game/Heroes/Hector/HectorShieldWallFactory.cs
@@ -9,23 +9,7 @@ public static class HectorShieldWallFactory
         root.transform.rotation = rotation;
 
         ShieldWallZone zone = root.AddComponent<ShieldWallZone>();
-        for (int i = -1; i <= 1; i++)
-            CreateShield(root.transform, i * 1.05f);
-
         Object.Destroy(root, zone.duration + .1f);
         return zone;
-    }
-
-    static void CreateShield(Transform parent, float localX)
-    {
-        GameObject shield = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        shield.name = "Shield";
-        shield.transform.SetParent(parent, false);
-        shield.transform.localPosition = new Vector3(localX, .55f, 0f);
-        shield.transform.localScale = new Vector3(.9f, 1.1f, .28f);
-        TowerFactory.SetColor(shield, new Color(.70f, .50f, .18f));
-
-        Collider collider = shield.GetComponent<Collider>();
-        if (collider != null) Object.Destroy(collider);
     }
 }
