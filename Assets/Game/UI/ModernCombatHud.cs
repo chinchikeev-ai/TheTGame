f38029fe66f60f5c9f597d9626356a33bdaae947
chinchikeev-ai <chinchikeev@gmail.com>
2026-09-14 00:29:36 +0800

    fix: recompose combat HUD into fixed zones

--- Assets/Game/UI/ModernCombatHud.cs
+++ Assets/Game/UI/ModernCombatHud.cs
@@ -24,6 +24,7 @@ public sealed class ModernCombatHud : MonoBehaviour
     Text buildSelectionText;
 
     GameObject buildTooltip;
+    GameObject selectedCard;
     Image tooltipAccent;
     Text tooltipTitle, tooltipRole, tooltipStats, tooltipMatchup;
     TowerType hoveredBuildType;
@@ -79,56 +80,55 @@ public sealed class ModernCombatHud : MonoBehaviour
         group = root.AddComponent<CanvasGroup>();
 
         BuildTopBar(root.transform);
-        BuildActionPanel(root.transform);
         BuildWaveBar(root.transform);
+        BuildActionPanel(root.transform);
         BuildDock(root.transform);
         BuildBuildTooltip(root.transform);
         BuildSelectedCard(root.transform);
-        BuildPcHints(root.transform);
     }
 
     void BuildTopBar(Transform parent)
     {
-        GameObject bar = Panel(parent, "TopResources", new Vector2(24, -24), new Vector2(660, 74), new Color(.035f, .022f, .016f, .92f), new Vector2(0, 1), new Vector2(0, 1));
-        Image coin = Icon(bar.transform, "CoinIcon", new Vector2(34, 0), new Vector2(38, 38), CoinSprite());
+        GameObject bar = Panel(parent, "TopResources", new Vector2(24, -24), new Vector2(470, 74), new Color(.035f, .022f, .016f, .92f), new Vector2(0, 1), new Vector2(0, 1));
+        Image coin = Icon(bar.transform, "CoinIcon", new Vector2(-194, 0), new Vector2(34, 34), CoinSprite());
         coin.color = Color.white;
-        goldText = Text(bar.transform, "0", new Vector2(82, 0), new Vector2(110, 60), 24, new Color(1f, .73f, .24f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        gateText = Text(bar.transform, "GATE", new Vector2(220, 0), new Vector2(190, 60), 20, new Color(.94f, .84f, .67f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        aliveText = Text(bar.transform, "ALIVE", new Vector2(425, 0), new Vector2(200, 60), 20, new Color(.94f, .84f, .67f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-    }
-
-    void BuildActionPanel(Transform parent)
-    {
-        GameObject panel = Panel(parent, "CombatActions", new Vector2(-24, -24), new Vector2(430, 190), new Color(.035f, .022f, .016f, .92f), new Vector2(1, 1), new Vector2(1, 1));
-        Button(panel.transform, "-", new Vector2(-178, -32), new Vector2(54, 54), DecreaseSpeed, false);
-        Button(panel.transform, "+", new Vector2(-58, -32), new Vector2(54, 54), IncreaseSpeed, false);
-        speedText = Text(panel.transform, "1x", new Vector2(-118, -32), new Vector2(82, 54), 18, new Color(1f, .78f, .34f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
-        magicButton = Button(panel.transform, "", new Vector2(110, -32), new Vector2(180, 54), () => GameManager.Instance?.UseMagic(), true);
-        magicText = magicButton.GetComponentInChildren<Text>();
-        giftButton = Button(panel.transform, "", new Vector2(110, -100), new Vector2(300, 50), () => GameManager.Instance?.UseGift(), false);
-        giftText = giftButton.GetComponentInChildren<Text>();
+        goldText = Text(bar.transform, "0", new Vector2(-148, 0), new Vector2(86, 58), 23, new Color(1f, .73f, .24f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        gateText = Text(bar.transform, "GATE", new Vector2(-28, 0), new Vector2(160, 58), 17, new Color(.94f, .84f, .67f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        aliveText = Text(bar.transform, "ALIVE", new Vector2(142, 0), new Vector2(160, 58), 17, new Color(.94f, .84f, .67f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
     }
 
     void BuildWaveBar(Transform parent)
     {
-        GameObject bar = Panel(parent, "WaveStatus", new Vector2(0, -24), new Vector2(860, 142), new Color(.035f, .022f, .016f, .95f), new Vector2(.5f, 1), new Vector2(.5f, 1));
-        waveText = Text(bar.transform, "WAVE", new Vector2(-90, 48), new Vector2(620, 34), 22, new Color(1f, .75f, .32f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
-        threatText = Text(bar.transform, "THREAT", new Vector2(-90, 12), new Vector2(620, 30), 15, new Color(.86f, .76f, .64f, 1f), TextAnchor.MiddleCenter, FontStyle.Normal);
-        wavePreviewText = Text(bar.transform, "", new Vector2(-90, -28), new Vector2(620, 34), 14, new Color(.93f, .82f, .67f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
-        startWaveButton = Button(bar.transform, L("START WAVE", "НАЧАТЬ ВОЛНУ"), new Vector2(330, 2), new Vector2(170, 82), StartWave, true);
+        GameObject bar = Panel(parent, "WaveStatus", new Vector2(0, -24), new Vector2(760, 124), new Color(.035f, .022f, .016f, .95f), new Vector2(.5f, 1), new Vector2(.5f, 1));
+        waveText = Text(bar.transform, "WAVE", new Vector2(-72, 40), new Vector2(500, 30), 20, new Color(1f, .75f, .32f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
+        threatText = Text(bar.transform, "THREAT", new Vector2(-72, 8), new Vector2(500, 28), 13, new Color(.86f, .76f, .64f, 1f), TextAnchor.MiddleCenter, FontStyle.Normal);
+        wavePreviewText = Text(bar.transform, "", new Vector2(-72, -24), new Vector2(500, 28), 12, new Color(.93f, .82f, .67f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
+        startWaveButton = Button(bar.transform, L("START WAVE", "НАЧАТЬ ВОЛНУ"), new Vector2(292, 0), new Vector2(150, 74), StartWave, true);
+    }
+
+    void BuildActionPanel(Transform parent)
+    {
+        GameObject panel = Panel(parent, "CombatActions", new Vector2(-24, -24), new Vector2(350, 132), new Color(.035f, .022f, .016f, .92f), new Vector2(1, 1), new Vector2(1, 1));
+        Button(panel.transform, "-", new Vector2(-142, 24), new Vector2(44, 44), DecreaseSpeed, false);
+        speedText = Text(panel.transform, "1x", new Vector2(-94, 24), new Vector2(52, 44), 16, new Color(1f, .78f, .34f, 1f), TextAnchor.MiddleCenter, FontStyle.Bold);
+        Button(panel.transform, "+", new Vector2(-46, 24), new Vector2(44, 44), IncreaseSpeed, false);
+        magicButton = Button(panel.transform, "", new Vector2(78, 24), new Vector2(170, 44), () => GameManager.Instance?.UseMagic(), true);
+        magicText = magicButton.GetComponentInChildren<Text>();
+        giftButton = Button(panel.transform, "", new Vector2(0, -34), new Vector2(300, 38), () => GameManager.Instance?.UseGift(), false);
+        giftText = giftButton.GetComponentInChildren<Text>();
     }
 
     void BuildDock(Transform parent)
     {
-        GameObject dock = Panel(parent, "BuildDock", new Vector2(0, 24), new Vector2(1120, 154), new Color(.035f, .022f, .016f, .95f), new Vector2(.5f, 0), new Vector2(.5f, 0));
-        Text(dock.transform, L("TROJAN DEFENSES", "ОБОРОНА ТРОИ"), new Vector2(-480, 54), new Vector2(260, 28), 14, new Color(.74f, .65f, .55f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        buildSelectionText = Text(dock.transform, "", new Vector2(390, 54), new Vector2(620, 28), 14, new Color(1f, .72f, .28f, 1f), TextAnchor.MiddleRight, FontStyle.Bold);
+        GameObject dock = Panel(parent, "BuildDock", new Vector2(0, 24), new Vector2(1040, 148), new Color(.035f, .022f, .016f, .95f), new Vector2(.5f, 0), new Vector2(.5f, 0));
+        Text(dock.transform, L("TROJAN DEFENSES", "ОБОРОНА ТРОИ"), new Vector2(-438, 52), new Vector2(240, 26), 13, new Color(.74f, .65f, .55f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        buildSelectionText = Text(dock.transform, "", new Vector2(310, 52), new Vector2(560, 26), 12, new Color(1f, .72f, .28f, 1f), TextAnchor.MiddleRight, FontStyle.Bold);
 
         for (int i = 0; i < buildTypes.Length; i++)
         {
             TowerType type = buildTypes[i];
-            float x = -430 + i * 174;
-            buildButtons[i] = Button(dock.transform, BuildLabel(type, buildHotkeys[i]), new Vector2(x, -18), new Vector2(158, 82), () => SelectBuild(type), false);
+            float x = -397 + i * 158;
+            buildButtons[i] = Button(dock.transform, BuildLabel(type, buildHotkeys[i]), new Vector2(x, -17), new Vector2(144, 78), () => SelectBuild(type), false);
             BuildButtonHoverRelay relay = buildButtons[i].gameObject.AddComponent<BuildButtonHoverRelay>();
             relay.Initialize(type, () => ShowBuildTooltip(type), HideBuildTooltip);
         }
@@ -136,7 +136,7 @@ public sealed class ModernCombatHud : MonoBehaviour
 
     void BuildBuildTooltip(Transform parent)
     {
-        buildTooltip = Panel(parent, "BuildHoverTooltip", new Vector2(0, 344), new Vector2(520, 174), new Color(.028f, .018f, .014f, .985f), new Vector2(.5f, 0), new Vector2(.5f, 0));
+        buildTooltip = Panel(parent, "BuildHoverTooltip", new Vector2(0, 210), new Vector2(500, 166), new Color(.028f, .018f, .014f, .985f), new Vector2(.5f, 0), new Vector2(.5f, 0));
         buildTooltip.GetComponent<Image>().raycastTarget = false;
 
         GameObject accentObject = new GameObject("Accent");
@@ -146,31 +146,26 @@ public sealed class ModernCombatHud : MonoBehaviour
         RectTransform accentRt = tooltipAccent.rectTransform;
         accentRt.anchorMin = accentRt.anchorMax = accentRt.pivot = new Vector2(0f, .5f);
         accentRt.anchoredPosition = new Vector2(8f, 0f);
-        accentRt.sizeDelta = new Vector2(8f, 154f);
+        accentRt.sizeDelta = new Vector2(8f, 146f);
 
-        tooltipTitle = Text(buildTooltip.transform, "", new Vector2(-116, 55), new Vector2(300, 30), 20, new Color(1f, .76f, .31f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        tooltipRole = Text(buildTooltip.transform, "", new Vector2(-116, 26), new Vector2(300, 28), 13, new Color(.78f, .70f, .61f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        tooltipStats = Text(buildTooltip.transform, "", new Vector2(-116, -10), new Vector2(300, 38), 13, new Color(.93f, .87f, .79f, 1f), TextAnchor.MiddleLeft, FontStyle.Normal);
-        tooltipMatchup = Text(buildTooltip.transform, "", new Vector2(126, -2), new Vector2(220, 112), 13, new Color(.78f, .91f, .70f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        tooltipTitle = Text(buildTooltip.transform, "", new Vector2(-108, 52), new Vector2(286, 28), 18, new Color(1f, .76f, .31f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        tooltipRole = Text(buildTooltip.transform, "", new Vector2(-108, 24), new Vector2(286, 26), 12, new Color(.78f, .70f, .61f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        tooltipStats = Text(buildTooltip.transform, "", new Vector2(-108, -10), new Vector2(286, 36), 12, new Color(.93f, .87f, .79f, 1f), TextAnchor.MiddleLeft, FontStyle.Normal);
+        tooltipMatchup = Text(buildTooltip.transform, "", new Vector2(118, -2), new Vector2(208, 106), 12, new Color(.78f, .91f, .70f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
         buildTooltip.SetActive(false);
     }
 
     void BuildSelectedCard(Transform parent)
     {
-        GameObject card = Panel(parent, "SelectedTowerCard", new Vector2(-24, -230), new Vector2(450, 430), new Color(.045f, .027f, .018f, .97f), new Vector2(1, 1), new Vector2(1, 1));
-        selectedTitle = Text(card.transform, L("SELECT A DEFENSE", "ВЫБЕРИТЕ ОБОРОНУ"), new Vector2(24, -22), new Vector2(400, 46), 24, new Color(1f, .70f, .28f, 1f), TextAnchor.UpperLeft, FontStyle.Bold);
-        selectedStats = Text(card.transform, "", new Vector2(24, -82), new Vector2(400, 138), 17, new Color(.94f, .87f, .77f, 1f), TextAnchor.UpperLeft, FontStyle.Normal);
-        selectedUpgradePreview = Text(card.transform, "", new Vector2(24, -214), new Vector2(400, 68), 15, new Color(1f, .73f, .31f, 1f), TextAnchor.UpperLeft, FontStyle.Bold);
-        selectedPriority = Text(card.transform, "", new Vector2(24, -278), new Vector2(400, 34), 15, new Color(.82f, .72f, .62f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
-        upgradeButton = Button(card.transform, L("UPGRADE", "УЛУЧШИТЬ"), new Vector2(-110, -342), new Vector2(190, 58), () => placement?.UpgradeSelected(), true);
-        sellButton = Button(card.transform, L("SELL", "ПРОДАТЬ"), new Vector2(110, -342), new Vector2(190, 58), () => placement?.SellSelected(), false);
-        priorityButton = Button(card.transform, L("TARGET PRIORITY", "ПРИОРИТЕТ ЦЕЛИ"), new Vector2(0, -397), new Vector2(410, 44), () => placement?.CycleSelectedPriority(), false);
-    }
-
-    void BuildPcHints(Transform parent)
-    {
-        GameObject hint = Panel(parent, "PcHints", new Vector2(24, 24), new Vector2(650, 48), new Color(.03f, .02f, .015f, .82f), new Vector2(0, 0), new Vector2(0, 0));
-        Text(hint.transform, L("LMB SELECT/BUILD   •   RMB HECTOR MOVE   •   Q E R F ABILITIES   •   ESC PAUSE", "ЛКМ ВЫБОР/СТРОЙКА   •   ПКМ ДВИЖЕНИЕ ГЕКТОРА   •   Q E R F СПОСОБНОСТИ   •   ESC ПАУЗА"), new Vector2(14, 0), new Vector2(620, 42), 13, new Color(.77f, .69f, .60f, 1f), TextAnchor.MiddleLeft, FontStyle.Normal);
+        selectedCard = Panel(parent, "SelectedTowerCard", new Vector2(-24, 200), new Vector2(390, 390), new Color(.045f, .027f, .018f, .97f), new Vector2(1, 0), new Vector2(1, 0));
+        selectedTitle = Text(selectedCard.transform, "", new Vector2(0, 145), new Vector2(350, 42), 21, new Color(1f, .70f, .28f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        selectedStats = Text(selectedCard.transform, "", new Vector2(0, 68), new Vector2(350, 110), 15, new Color(.94f, .87f, .77f, 1f), TextAnchor.UpperLeft, FontStyle.Normal);
+        selectedUpgradePreview = Text(selectedCard.transform, "", new Vector2(0, -22), new Vector2(350, 62), 13, new Color(1f, .73f, .31f, 1f), TextAnchor.UpperLeft, FontStyle.Bold);
+        selectedPriority = Text(selectedCard.transform, "", new Vector2(0, -72), new Vector2(350, 30), 13, new Color(.82f, .72f, .62f, 1f), TextAnchor.MiddleLeft, FontStyle.Bold);
+        upgradeButton = Button(selectedCard.transform, L("UPGRADE", "УЛУЧШИТЬ"), new Vector2(-92, -126), new Vector2(166, 52), () => placement?.UpgradeSelected(), true);
+        sellButton = Button(selectedCard.transform, L("SELL", "ПРОДАТЬ"), new Vector2(92, -126), new Vector2(166, 52), () => placement?.SellSelected(), false);
+        priorityButton = Button(selectedCard.transform, L("TARGET PRIORITY", "ПРИОРИТЕТ ЦЕЛИ"), new Vector2(0, -170), new Vector2(350, 38), () => placement?.CycleSelectedPriority(), false);
+        selectedCard.SetActive(false);
     }
 
     void Update()
@@ -189,6 +184,7 @@ public sealed class ModernCombatHud : MonoBehaviour
         if (blocked || GameManager.Instance == null)
         {
             HideBuildTooltip();
+            if (selectedCard != null) selectedCard.SetActive(false);
             return;
         }
 
@@ -216,8 +212,8 @@ public sealed class ModernCombatHud : MonoBehaviour
     {
         GameManager gm = GameManager.Instance;
         goldText.text = gm.Money.ToString();
-        gateText.text = $"{L("GATE", "ВОРОТА")}   {gm.BaseHealth}/{gm.MaxBaseHealth}";
-        aliveText.text = $"{L("ENEMIES", "ВРАГИ")}   {EnemyRegistry.AliveCount}";
+        gateText.text = $"{L("GATE", "ВОРОТА")} {gm.BaseHealth}/{gm.MaxBaseHealth}";
+        aliveText.text = $"{L("ENEMIES", "ВРАГИ")} {EnemyRegistry.AliveCount}";
     }
 
     void UpdateActions()
@@ -228,13 +224,13 @@ public sealed class ModernCombatHud : MonoBehaviour
 
         float cooldown = gm != null ? gm.MagicCooldownRemaining : 0f;
         magicText.text = cooldown > 0f
-            ? L("MAGIC ", "МАГИЯ ") + Mathf.CeilToInt(cooldown) + L("s", "с")
-            : L("MAGIC READY", "МАГИЯ ГОТОВА");
+            ? L("POWER ", "СИЛА ") + Mathf.CeilToInt(cooldown) + L("s", "с")
+            : L("POWER READY", "СИЛА ГОТОВА");
         magicButton.interactable = gm != null && cooldown <= 0f && !gm.GameEnded;
 
         giftText.text = gm != null && gm.GiftAvailable
-            ? L("GIFT +100 / +2", "ДАР +100 / +2")
-            : L("GIFT USED", "ДАР ИСПОЛЬЗОВАН");
+            ? L("DIVINE GIFT +100 / +2", "ДАР БОГА +100 / +2")
+            : L("DIVINE GIFT USED", "ДАР БОГА ИСПОЛЬЗОВАН");
         giftButton.interactable = gm != null && gm.GiftAvailable && !gm.GameEnded;
     }
 
@@ -242,7 +238,7 @@ public sealed class ModernCombatHud : MonoBehaviour
     {
         GameManager gm = GameManager.Instance;
         int sec = spawner != null ? Mathf.RoundToInt(spawner.CurrentWaveElapsed) : 0;
-        waveText.text = $"{L("CHAPTER I", "ГЛАВА I")}   •   {L("WAVE", "ВОЛНА")} {gm.CurrentWave}/{gm.MaxWaves}   •   {sec / 60:00}:{sec % 60:00}";
+        waveText.text = $"{L("CHAPTER I", "ГЛАВА I")} • {L("WAVE", "ВОЛНА")} {gm.CurrentWave}/{gm.MaxWaves} • {sec / 60:00}:{sec % 60:00}";
 
         if (spawner == null)
         {
@@ -259,11 +255,11 @@ public sealed class ModernCombatHud : MonoBehaviour
                 : L("STANDARD ENEMY FORMATION", "ОБЫЧНАЯ ВРАЖЕСКАЯ ФОРМАЦИЯ");
 
         if (spawner.WaveActive)
-            threatText.text = $"{L("WAVE ACTIVE", "ВОЛНА ИДЁТ")}   •   {EnemyRegistry.AliveCount} {L("alive", "в строю")}   •   {threat}";
+            threatText.text = $"{L("WAVE ACTIVE", "ВОЛНА ИДЁТ")} • {EnemyRegistry.AliveCount} {L("alive", "в строю")} • {threat}";
         else if (spawner.InterWaveCountdown > 0)
-            threatText.text = $"{L("NEXT WAVE IN", "СЛЕДУЮЩАЯ ВОЛНА ЧЕРЕЗ")} {Mathf.CeilToInt(spawner.InterWaveCountdown)}{L("s", "с")}   •   {spawner.NextWaveEnemyCount} {L("enemies", "врагов")}";
+            threatText.text = $"{L("NEXT WAVE IN", "СЛЕДУЮЩАЯ ВОЛНА ЧЕРЕЗ")} {Mathf.CeilToInt(spawner.InterWaveCountdown)}{L("s", "с")} • {spawner.NextWaveEnemyCount} {L("enemies", "врагов")}";
         else
-            threatText.text = $"{L("READY", "ГОТОВО")}   •   {spawner.NextWaveEnemyCount} {L("enemies", "врагов")}   •   {threat}";
+            threatText.text = $"{L("READY", "ГОТОВО")} • {spawner.NextWaveEnemyCount} {L("enemies", "врагов")} • {threat}";
 
         wavePreviewText.text = CombatHudWaveFormatter.BuildPreview(spawner);
         bool canStart = spawner.WaitingForManualStart && !spawner.WaveActive && !gm.GameEnded;
@@ -304,15 +300,20 @@ public sealed class ModernCombatHud : MonoBehaviour
         bool recommended = hoveredBuildType == RecommendedDefense();
         bool affordable = GameManager.Instance != null && GameManager.Instance.Money >= data.cost;
         tooltipAccent.color = recommended ? new Color(1f, .68f, .16f, 1f) : affordable ? new Color(.30f, .82f, .36f, 1f) : new Color(.82f, .18f, .08f, 1f);
-        tooltipTitle.text = TowerDisplayName(hoveredBuildType) + (recommended ? "   •   " + L("RECOMMENDED", "РЕКОМЕНДУЕТСЯ") : "");
+        tooltipTitle.text = TowerDisplayName(hoveredBuildType) + (recommended ? " • " + L("RECOMMENDED", "РЕКОМЕНДУЕТСЯ") : "");
         tooltipRole.text = TowerRole(hoveredBuildType);
         float dps = data.damage * Mathf.Max(.01f, data.attacksPerSecond);
-        tooltipStats.text = $"{L("DMG", "УРОН")} {data.damage:0.#}   •   DPS {dps:0.#}   •   {L("RNG", "ДАЛЬН")} {data.range:0.0}   •   {data.cost} {L("GOLD", "ЗОЛОТА")}";
+        tooltipStats.text = $"{L("DMG", "УРОН")} {data.damage:0.#} • DPS {dps:0.#} • {L("RNG", "ДАЛЬН")} {data.range:0.0} • {data.cost} {L("GOLD", "ЗОЛОТА")}";
         tooltipMatchup.text = $"+ {L("STRONG", "СИЛЁН")}\n{StrongAgainst(hoveredBuildType)}\n\n− {L("WEAK", "СЛАБ")}\n{WeakAgainst(hoveredBuildType)}";
     }
 
     void UpdateSelected()
     {
+        Tower selected = placement != null ? placement.SelectedTower : null;
+        bool visible = selected != null;
+        if (selectedCard != null) selectedCard.SetActive(visible);
+        if (!visible) return;
+
         CombatHudTowerPanelPresenter.RefreshSelected(
             placement,
             selectedTitle,
