17fcfc696d374269f90d8d7c7da9c27d28e5d87f
chinchikeev-ai <chinchikeev@gmail.com>
2026-09-14 16:40:49 +0800

    Add contract tests for minor gameplay UX fixes

---
+++ Assets/Tests/EditMode/MinorGameplayUxContractTests.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MinorGameplayUxContractTests
+{
+    [Test]
+    public void ChapterOne_FirstEncounter_HasThirtySecondPreparation()
+    {
+        ChapterData chapter = Resources.Load<ChapterData>("Chapters/Chapter01_Landing");
+        Assert.NotNull(chapter);
+        EncounterData encounter = chapter.GetEncounter(1);
+        Assert.NotNull(encounter);
+        Assert.AreEqual(30f, encounter.preparationTime, .01f);
+    }
+
+    [Test]
+    public void DivineGift_CanBeSelectedOnlyOncePerMap()
+    {
+        GameObject host = new GameObject("GiftContractTest");
+        GameManager manager = host.AddComponent<GameManager>();
+        try
+        {
+            int startingGold = manager.Money;
+            Assert.IsTrue(manager.GiftAvailable);
+            Assert.IsTrue(manager.UseGift(DivineGiftType.Apollo));
+            Assert.IsTrue(manager.GiftSelected);
+            Assert.AreEqual(DivineGiftType.Apollo, manager.SelectedGift);
+            Assert.AreEqual(startingGold + 100, manager.Money);
+            Assert.IsFalse(manager.GiftAvailable);
+            Assert.IsFalse(manager.UseGift(DivineGiftType.Ares), "A second patron selection on the same map must be rejected.");
+            Assert.AreEqual(DivineGiftType.Apollo, manager.SelectedGift);
+        }
+        finally
+        {
+            Object.DestroyImmediate(host);
+        }
+    }
+
+    [Test]
+    public void HectorRouteNavigator_ProjectsOffRoadCommandsOntoRoute()
+    {
+        GameObject root = new GameObject("RouteTest");
+        try
+        {
+            Transform a0 = Point(root.transform, "A0", new Vector3(0f, .6f, 0f));
+            Transform a1 = Point(root.transform, "A1", new Vector3(5f, .6f, 0f));
+            Transform a2 = Point(root.transform, "A2", new Vector3(10f, .6f, 0f));
+            Transform[][] routes = { new[] { a0, a1, a2 } };
+
+            Vector3 projected = HectorRouteNavigator.ProjectToNearestRoute(new Vector3(4f, .6f, 7f), routes, .6f);
+            Assert.AreEqual(4f, projected.x, .01f);
+            Assert.AreEqual(0f, projected.z, .01f);
+
+            List<Vector3> path = new List<Vector3>();
+            Assert.IsTrue(HectorRouteNavigator.BuildPath(a0.position, new Vector3(9f, .6f, 6f), routes, .6f, path));
+            Assert.Greater(path.Count, 0);
+            Assert.AreEqual(0f, path[path.Count - 1].z, .01f);
+        }
+        finally
+        {
+            Object.DestroyImmediate(root);
+        }
+    }
+
+    [Test]
+    public void HectorRouteNavigator_GateStartUsesRoadPointBeforeGate()
+    {
+        GameObject root = new GameObject("GateRouteTest");
+        try
+        {
+            Transform p0 = Point(root.transform, "P0", new Vector3(0f, .6f, 0f));
+            Transform p1 = Point(root.transform, "UnderGate", new Vector3(5f, .6f, 0f));
+            Transform p2 = Point(root.transform, "Gate", new Vector3(6.5f, .6f, 0f));
+            Transform[][] routes = { new[] { p0, p1, p2 } };
+
+            Vector3 start = HectorRouteNavigator.GateStart(routes, .6f);
+            Assert.AreEqual(p1.position.x, start.x, .01f);
+            Assert.AreEqual(p1.position.z, start.z, .01f);
+        }
+        finally
+        {
+            Object.DestroyImmediate(root);
+        }
+    }
+
+    static Transform Point(Transform parent, string name, Vector3 position)
+    {
+        GameObject go = new GameObject(name);
+        go.transform.SetParent(parent, false);
+        go.transform.position = position;
+        return go.transform;
+    }
+}
