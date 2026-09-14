4b63d44f566294e9642f8a89962793088b2bfc86
chinchikeev-ai <chinchikeev@gmail.com>
2026-09-14 17:00:28 +0800

    test: cover screenshot button placement

--- Assets/Tests/EditMode/GameScreenshotControllerTests.cs
+++ Assets/Tests/EditMode/GameScreenshotControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using UnityEngine;
 
 public class GameScreenshotControllerTests
 {
@@ -10,5 +11,15 @@ public class GameScreenshotControllerTests
         string fileName = GameScreenshotController.BuildFileName(timestamp);
 
         Assert.AreEqual("TheTroyGame_2026-09-14_16-35-42-123.png", fileName);
+    }
+
+    [Test]
+    public void ScreenshotButton_IsAnchoredRightCenterInsideViewport()
+    {
+        Assert.AreEqual(new Vector2(1f, .5f), GameScreenshotController.CaptureButtonAnchor);
+        Assert.Less(GameScreenshotController.CaptureButtonOffset.x, 0f);
+        Assert.AreEqual(0f, GameScreenshotController.CaptureButtonOffset.y);
+        Assert.Greater(GameScreenshotController.CaptureButtonSize.x, 0f);
+        Assert.Greater(GameScreenshotController.CaptureButtonSize.y, 0f);
     }
 }
