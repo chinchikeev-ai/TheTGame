de229d4f60134ed997ee598ee2f993df1d3520d1
chinchikeev-ai <chinchikeev@gmail.com>
2026-09-14 15:30:19 +0800

    Remove obsolete CampaignProgressUI compatibility shim

--- Assets/Game/UI/CampaignProgressUI.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-// Compatibility shell for older scene/script references.
-// End-of-battle presentation is owned by GameMenuController + ResultScreenPresentation.
-public class CampaignProgressUI : MonoBehaviour
-{
-}
